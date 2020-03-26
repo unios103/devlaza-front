1190c79655212da6c7c5a979c4036210a50394e9
loxygen <55672846+loxygenK@users.noreply.github.com>
2020-03-26 21:14:13 +0900

    †追加† ヘッダー(not レスポンシブ)ができた (#1)
    * †追加† ヘッダーができた
    
    * Update src/comp/Header.tsx
    
    #1 のレビューのSuggestionを適用。
    
    Co-Authored-By: かわえもん <34652535+kawaemon@users.noreply.github.com>
    
    * †編集† FontAwesome周りをライブラリに変更した
    
    * Update public/index.html
    
    #1 のsuggestionを適用
    
    Co-Authored-By: かわえもん <34652535+kawaemon@users.noreply.github.com>
    
    * †編集†ヘッダーのテキスト選択をテキストボックス以外できないようにした
    
    * †削除† 消し忘れがあった
    
    * †削除† npmとyarnを両立させていた
    
    Co-authored-by: かわえもん <34652535+kawaemon@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./comp/Header";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+
+library.add(fas, far)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<Header />
     </div>
   );
 }
