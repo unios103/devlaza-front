84b42a95a73bbb5c5ec8221b709ffe348fda82ba
loxygenk <loxygen.k@gmail.com>
2020-03-30 22:29:21 +0900

    †追加† メインページの基本的なものを作った

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import Header from "./comp/Header";
 
@@ -6,12 +7,19 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 
+import * as Pages from "./comp/pages"
+
 library.add(fas, far)
 
 function App() {
   return (
     <div className="App">
 			<Header />
+			<div className="app-content-wrapper">
+				<Router>
+					<Route exact path="/" component={Pages.MainPage} />
+				</Router>
+			</div>
     </div>
   );
 }
