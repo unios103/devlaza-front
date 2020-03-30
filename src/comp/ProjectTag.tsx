5df189406ca84e287ec335375fccec7ede5ae8e9
loxygenk <loxygen.k@gmail.com>
2020-03-30 15:35:31 +0900

    †追加† メインページ用のプロジェクトリストのコンポーネントを作った

---
+++ src/comp/ProjectTag.tsx
@@ -0,0 +1,33 @@
+import * as React from "react"
+import "./ProjectTag.css"
+
+export type ProjectTagProp = {
+	tag_id: string
+}
+
+type ProjectTagState = {
+	name: string
+}
+
+class ProjectTag extends React.Component<ProjectTagProp, ProjectTagState> {
+
+	constructor(props: ProjectTagProp){
+		super(props);
+
+		this.state = {
+			name: "タグ: " + this.props.tag_id
+		}
+
+	}
+
+	render() {
+		return (
+			<div className="ptag-wrapper">
+				{this.state.name}
+			</div>
+		)
+	}
+
+}
+
+export default ProjectTag
